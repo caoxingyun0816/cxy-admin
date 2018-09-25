868a2c11354372e19927e03ef63d74074079b527
caoxingyun <1194441452@qq.com>
2018-09-25 15:04:28 +0800

    test

---
+++ src/test/java/com/imooc/thinkinjava/lesson2/Test1.java
@@ -0,0 +1,24 @@
+package com.imooc.thinkinjava.lesson2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by caoxingyun on 2018/9/20.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Test1 {
+
+    @Test
+    public void test1(){
+        char a;
+        int i;
+        long j;
+        System.out.println( i);
+        System.out.println(j);
+        System.out.println(a);
+    }
+}
